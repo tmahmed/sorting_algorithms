e9c846642278b8c9c89eb0843756b310c6b15353
Tanzim Mousfiq Ahmed <tanzimmahmed@gmail.com>
2021-02-07 19:26:43 +0600

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(x):
+
+    for i in range(len(x) - 1):
+        for j in range(len(x) - i - 1):
+            if x[j] > x[j + 1]: x[j], x[j + 1] = x[j + 1], x[j]
+
+    for i in range(len(x)):
+        print(x[i])
+
+
+bubble_sort([5, 4, 3, 2, 1])
